c8edd3b83b83876140e75d3c6b8ee7d088f29125
Andrey <122050567+ttsshhl@users.noreply.github.com>
2026-02-07 20:53:44 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://znsrmhtcxnpiihbxswdj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpuc3JtaHRjeG5waWloYnhzd2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMjkyMzMsImV4cCI6MjA4NTcwNTIzM30.WXLXd1mpRjwhc4AFeOOjrTAE4iMj2xvW2iKslgOn6t0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
